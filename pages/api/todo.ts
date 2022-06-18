2c9b2f6d27b43d34652b43bb2866665258854b03
David Pamerean Budiarto <davidpamereanb@gmail.com>
2022-06-18 22:07:20 +0700

    fix: missing return

--- pages/api/todo.ts
+++ pages/api/todo.ts
@@ -12,7 +12,7 @@ export default async function handler(
       case "GET": {
         const tasks = await prisma.task.findMany();
 
-        res.status(200).json({
+        return res.status(200).json({
           tasks,
         });
       }
